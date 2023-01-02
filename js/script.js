f1bf13c01d57f626bdbcdf799f92312131da265d
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-01-01 23:32:42 -0300

    criando rascunho do JS

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// 1 Quando digitar letra, subsitutir por código
+
+// function letraCripto (letra){
+//  document.write(letra);   
+//}
+
+// 2 function textoAtivo (texto) {
+// 3 Receba texto,
+// 4 Por uma outra função interna function {
+// Botão criptografar, dará o comando para alterar o texto    
+//}
+// 5 Jogar no lugar da img e da frase Nenhuma mensagem...    
+//}
+
+// 5 fuction botaoCopiarAtivo {
+// Com o texto ativado na região da img, substituir texto Digite um texto..., por botão copiar   
+//}
+// 6 Botão descriptografar
+// function (textoOriginal) {
+//  Volta ao texto original novamente  
+//}
