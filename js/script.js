0bceca617eaf479ae0e011c27a1019377bb3022a
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-01-02 21:15:12 -0300

    testando funções no JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,74 @@
 // 1 Quando digitar letra, subsitutir por código
-
+//As "chaves" de criptografia que utilizaremos são:
+//Se a letra "e" substitua por "enter"
+//Se a letra "i" é substitua por "imes"
+//Se a letra "a" é substitua por  "ai"
+//Se a letra "o" é substitua por "ober"
+//Se a letra "u" é substitua por "ufat"
 // function letraCripto (letra){
 //  document.write(letra);   
 //}
 
+// se letra igual a..., substitua por....
+var texto = prompt("");
+
+// descrição de cada letra
+function criptografa (letra) {
+    if (letra == "e"){
+        document.write("enter")
+    }
+};
+
+function criptografa (letra) {
+    if (letra == "i"){
+        document.write("imes")
+    }
+};
+
+function criptografa (letra) {
+    if (letra == "a"){
+        document.write("ai")
+    }
+};
+
+function criptografa (letra) {
+    if (letra == "o"){
+        document.write("ober")
+    }
+};
+
+function criptografa (letra) {
+    if (letra == "u"){
+        document.write("ufat")
+    }
+};
+
 // 2 function textoAtivo (texto) {
+    function mostra(textoEntrada) {
+
+        document.write(textoEntrada);
+        
+};
 // 3 Receba texto,
+document.write("");
+
+
 // 4 Por uma outra função interna function {
 // Botão criptografar, dará o comando para alterar o texto    
 //}
+
+return texto;
+
 // 5 Jogar no lugar da img e da frase Nenhuma mensagem...    
 //}
 
 // 5 fuction botaoCopiarAtivo {
 // Com o texto ativado na região da img, substituir texto Digite um texto..., por botão copiar   
 //}
+
 // 6 Botão descriptografar
 // function (textoOriginal) {
 //  Volta ao texto original novamente  
+
+
 //}
