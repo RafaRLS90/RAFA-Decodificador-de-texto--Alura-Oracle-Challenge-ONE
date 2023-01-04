f14591ac7f83b491b337b76df1771a7c28b397f6
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-01-04 20:55:19 -0300

    Adicionando README

---
+++ README.md
@@ -0,0 +1,36 @@
+# Rascunho do JS
+
+// 1 Quando digitar letra, subsitutir por código
+//As "chaves" de criptografia que utilizaremos são:
+//Se a letra "e" substitua por "enter"
+//Se a letra "i" é substitua por "imes"
+//Se a letra "a" é substitua por  "ai"
+//Se a letra "o" é substitua por "ober"
+//Se a letra "u" é substitua por "ufat"
+// function letraCripto (letra){
+//  document.write(letra);   
+//}
+
+// se letra igual a..., substitua por....
+
+// descrição de cada letra
+
+
+// 2 function textoAtivo (texto) {
+    
+// 3 Receba texto,
+
+// 4 Por uma outra função interna function {
+// Botão criptografar, dará o comando para alterar o texto    
+//} O segundo input receberá por uma função o valor de texto entrada
+
+// 5 Jogar no lugar da img e da frase Nenhuma mensagem...    
+//}
+
+// 5 fuction botaoCopiarAtivo {
+// Com o texto ativado na região da img, substituir texto Digite um texto..., por botão copiar   
+//}
+
+// 6 Botão descriptografar
+// function (textoOriginal) {
+//  Volta ao texto original novamente  //}
--- js/script.js
+++ js/script.js
@@ -1,81 +1,46 @@
-// 1 Quando digitar letra, subsitutir por código
-//As "chaves" de criptografia que utilizaremos são:
-//Se a letra "e" substitua por "enter"
-//Se a letra "i" é substitua por "imes"
-//Se a letra "a" é substitua por  "ai"
-//Se a letra "o" é substitua por "ober"
-//Se a letra "u" é substitua por "ufat"
-// function letraCripto (letra){
-//  document.write(letra);   
-//}
-
-// se letra igual a..., substitua por....
-var texto = document.querySelector("input");
-
-// descrição de cada letra
 
 
-// 2 function textoAtivo (texto) {
-    function mostra(textoEntrada) {
 
-        document.write("input");
-        
-};
-// 3 Receba texto,
+var textoEntrada = document.querySelector("caixa-texto");
+
+
 document.write("");
 
 
-// 4 Por uma outra função interna function {
-// Botão criptografar, dará o comando para alterar o texto    
-//}
+
+function mostra(textoEntrada) {
+
+    document.write("input");
+    
+};
+
 var texto = document.querySelector("input");
 
 function criptografar (texto) {
-    if (letra == "e"){
-        document.write("enter")
+    if (input.value == "e"){
+        print.textoEntrada("enter");
     }
-};
-
-function criptografar (texto) {
-    if (letra == "i"){
-        document.write("imes")
+    if (input.value == "i"){
+        print.textoEntrada("imes");
     }
-};
-
-function criptografar (texto) {
-    if (letra == "a"){
-        document.write("ai")
+    if (input.value == "a"){
+        print.textoEntrada("ai");
     }
-};
-
-function criptografar (texto) {
-    if (letra == "o"){
-        document.write("ober")
+    if (input.value == "o"){
+        print.textoEntrada("ober");
     }
-};
-
-function criptografar (letra) {
-    if (letra == "u"){
-        document.write("ufat")
+    if (InputEvent == "u"){
+        print.textoEntrada("ufat");
     }
-
-    InputDeviceInfo.value = "texto";
 };
 
 criptografa.onclick = criptografar;
 
+return textoEntrada;
 
 
-// 5 Jogar no lugar da img e da frase Nenhuma mensagem...    
-//}
 
-// 5 fuction botaoCopiarAtivo {
-// Com o texto ativado na região da img, substituir texto Digite um texto..., por botão copiar   
-//}
 
-// 6 Botão descriptografar
-// function (textoOriginal) {
-//  Volta ao texto original novamente  
 var acaoBotaoDescriptografa = document.querySelector("descriptografa");
 
 function descriptografar (){
@@ -84,4 +49,3 @@ function descriptografar (){
 
 descriptografa.onclick = descriptografar;
 
-//}
