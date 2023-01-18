6fcece0e7fc1d77d2d506358b0aa877572221e11
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-01-17 23:54:25 -0300

    função criptografar, funcionando

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 var textoEntrada = document.querySelector("#input-padrao");
 var textoSaida = document.querySelector("#texto-codificado");
 
-
-function criptografar () {
    // O método toLowerCase() retorna o valor da string que foi chamada convertido para minúsculo.
    // i afeta tanto minusculas, quanto maísculas
    // g afeta toda linha
    // m afeta linha e parágrafo
-   
+function criptografar () {
+    
     var texto = document.getElementById("input-padrao").value.toLowerCase();
     
     var transformCripto = texto.replace(/e/igm, "enter");
@@ -17,26 +16,28 @@ function criptografar () {
     var transformCripto = transformCripto.replace(/u/igm, "ufat");
     
     
-    document.getElementById("texto-secundario").innerHTML = texto;
-    console.log("texto")
+    document.getElementById("texto-codificado").innerHTML = transformCripto;
+    
+    document.getElementById("imagem-principal").style.display = "none";
+    document.getElementById("texto-codificado").style.display = "block";
 }
 
 
 function descriptografar (){
     var texto = document.getElementById("input-padrao").value.toLowerCase();
 
-    var transformDescripto = texto.replace(/enter/igm, "e");
-    var transformDescripto = transformDescripto.replace(/imes/igm,"i");
-    var transformDescripto = transformDescripto.replace(/ai/igm, "a");
-    var transformDescripto = transformDescripto.replace(/ober/igm, "o");
-    var transformDescripto =  transformDescripto.replace(/ufat/igm, "u");
+    var desTransformDescripto = texto.replace(/enter/igm, "e");
+    var desTransformDescripto = desTransformDescripto.replace(/imes/igm,"i");
+    var desTransformDescripto = desTransformDescripto.replace(/ai/igm, "a");
+    var desTransformDescripto = desTransformDescripto.replace(/ober/igm, "o");
+    var desTransformDescripto =  desTransformDescripto.replace(/ufat/igm, "u");
    
     
-    
+    document.getElementById("texto-input").innerHTML = desTransformCripto;
 };
 
 function copiar(){
-    var copieTexto = document.getElementById('input-padrao');
+    var copieTexto = document.getElementById("texto-codificado");
 
     copieTexto.select();
     document.execCommand('copy');
